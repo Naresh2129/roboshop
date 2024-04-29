6b57a2b6c3bffdb15d24d2f7a6e16e4bbd26eba9
Naresh2129 <nareshboligorla@gmail.com>
2024-04-29 09:44:55 +0530

    using of non functional requirements in shell scripting,remove old app directory,applying redirector

--- user.sh
+++ user.sh
@@ -1,16 +1,44 @@
-cp user.service /etc/systemd/system/user.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-curl -sl https://rpm.nodesource.com/setup_lts.x | bash
-dnf install nodejs -y
-useradd roboshop
-mkdir /app
+log=/tmp/roboshop.log
+
+echo -e "\e[36m>>>>>>>>>>>>>create user service<<<<<<<<<<<\e[0m"
+cp catalogue.service /etc/systemd/system/user.service &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>>>create mongodb repo<<<<<<<<<<<\e[0m"
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>>>install nodejs repo<<<<<<<<<<<\e[0m"
+curl -sl https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>>>install nodejs<<<<<<<<<<<\e[0m"
+dnf install nodejs -y &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>>>create application user<<<<<<<<<<<\e[0m"
+useradd roboshop &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>>>create application directory<<<<<<<<<<<\e[0m"
+rm -rf /app &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>>>create application directory<<<<<<<<<<<\e[0m"
+mkdir /app &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>>>download application content<<<<<<<<<<<\e[0m"
 curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+
+echo -e "\e[36m>>>>>>>>>>>>>exatract application content<<<<<<<<<<<\e[0m"
 cd /app
-unzip /tmp/user.zip
+unzip /tmp/user.zip &>>${log}
 cd /app
-npm install
-dnf install mongodb-org-shell -y
-mongo --host mongodb.kdevops.online </app/schema/user.js
-systemctl daemon-reload
-systemctl enable user
-systemctl restart user
+
+echo -e "\e[36m>>>>>>>>>>>>>install nodejs dependencies<<<<<<<<<<<\e[0m"
+npm install &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>>>install mongo client<<<<<<<<<<<\e[0m"
+dnf install mongodb-org-shell -y &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>>>load user schema<<<<<<<<<<<\e[0m"
+mongo --host mongodb.nkdevops29.online </app/schema/user.js &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>>>start  user service<<<<<<<<<<<\e[0m"
+systemctl daemon-reload &>>${log}
+systemctl enable user &>>${log}
+systemctl restart user &>>${log}
