a57aa10b4d2b994a941ce4130466d20aa79a947a
Naresh2129 <nareshboligorla@gmail.com>
2024-05-01 08:43:43 +0530

    using of non functional requirements in shell scripting,remove old app directory,applying redirector conditions

--- common.sh
+++ common.sh
@@ -1,25 +1,42 @@
 log=/tmp/roboshop.log
 
+func_exit_status() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+        echo -e "\e[31m failure \e[0m"
+  fi
+}
 func_apppreq() {
     echo -e "\e[36m>>>>>>>>>>>>>create  ${component} service<<<<<<<<<<<\e[0m"
     cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+ func_exit_status
 
     echo -e "\e[36m>>>>>>>>>>>>>create application user<<<<<<<<<<<\e[0m"
+    id roboshop &>>${log}
+    if [ $? -ne 0 ]; then
     useradd roboshop &>>${log}
+    fi
+ func_exit_status
 
     echo -e "\e[36m>>>>>>>>>>>>>clean up existing application content<<<<<<<<<<<\e[0m"
     rm -rf /app &>>${log}
+ func_exit_status
 
     echo -e "\e[36m>>>>>>>>>>>>>create application directory<<<<<<<<<<<\e[0m"
     mkdir /app &>>${log}
+func_exit_status
 
     echo -e "\e[36m>>>>>>>>>>>>>download application content<<<<<<<<<<<\e[0m"
     curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+func_exit_status
 
     echo -e "\e[36m>>>>>>>>>>>>>exatract application content<<<<<<<<<<<\e[0m"
     cd /app
     unzip /tmp/${component}.zip &>>${log}
     cd /app
+func_exit_status
+
 }
 
 func_systemd() {
@@ -27,6 +44,7 @@ func_systemd() {
     systemctl daemon-reload &>>${log}
     systemctl enable ${component} &>>${log}
     systemctl restart ${component} &>>${log}
+ func_exit_status
 
 }
 
@@ -35,9 +53,11 @@ func_schema_setup() {
     echo -e "\e[36m>>>>>>>>>>>>>install mongo client<<<<<<<<<<<\e[0m" |tee -a /tmp/roboshop.log
     dnf install mongodb-org-shell -y &>>${log}
 
+
     echo -e "\e[36m>>>>>>>>>>>>>load ${component} schema<<<<<<<<<<<\e[0m" |tee -a /tmp/roboshop.log
     mongo --host mongodb.nkdevops29.online </app/schema/${component}.js &>>${log}
   fi
+  func_exit_status
 
   if [ "${schema_type}" == "mysql" ]; then
    echo -e "\e[36m>>>>>>>>>>>>>install my sql client <<<<<<<<<<<\e[0m"
@@ -46,7 +66,7 @@ func_schema_setup() {
    echo -e "\e[36m>>>>>>>>>>>>> load schema<<<<<<<<<<<\e[0m"
    mysql -h mysql.nkdevops29.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
   fi
-
+func_exit_status
 }
 
 func_nodejs() {
@@ -55,47 +75,52 @@ func_nodejs() {
 
   echo -e "\e[36m>>>>>>>>>>>>>create mongodb repo<<<<<<<<<<<\e[0m"
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
-
+func_exit_status
   echo -e "\e[36m>>>>>>>>>>>>>install nodejs repo<<<<<<<<<<<\e[0m"
   curl -sl https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
-
+func_exit_status
   echo -e "\e[36m>>>>>>>>>>>>>install nodejs<<<<<<<<<<<\e[0m"
   dnf install nodejs -y &>>${log}
-
+func_exit_status
   func_apppreq
 
   echo -e "\e[36m>>>>>>>>>>>>>download nodejs dependencies<<<<<<<<<<<\e[0m"
   npm install &>>${log}
-
+func_exit_status
   funnc_schema_setup
 
   func_systemd
-
+func_exit_status
  }
  func_java(){
 
   echo -e "\e[36m>>>>>>>>>>>>>install maven<<<<<<<<<<<\e[0m"
   dnf install maven -y
-
+func_exit_status
   func_apppreq
 
   echo -e "\e[36m>>>>>>>>>>>>> build ${component} service <<<<<<<<<<<\e[0m"
   mvn clean package
   mv target/shipping-1.0.jar shipping.jar &>>${log}
-
+func_exit_status
   func_schema_setup
 
   func_systemd
+  func_exit_status
 }
 
 func_python() {
   echo -e "\e[36m>>>>>>>>>>>>>build ${component} service <<<<<<<<<<<\e[0m"
   dnf install python36 gcc python3-devel -y &>>${log}
+func_exit_status
 
   func_apppreq
 
+  sed -i "s/rabbitmq_app_password/${rabbitmq_app_password}/" /etc/systemd/system/${component}.service
+
   echo -e "\e[36m>>>>>>>>>>>>> build ${component} service <<<<<<<<<<<\e[0m"
   pip3.6 install -r requirements.txt &>>${log}
+func_exit_status
 
   func_systemd
 }
--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,28 @@
-dnf install nginx -y
-cp nginx.roboshop.conf /etc/nginx/default.d/roboshop.conf
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+source common.sh
+
+echo -e "\e[36m>>>>>>>>>>>>>install nginx <<<<<<<<<<<\e[0m"
+dnf install nginx -y &>>${log}
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>> copy roboshop configuration<<<<<<<<<<<\e[0m"
+cp nginx.roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>clean up old content<<<<<<<<<<<\e[0m"
+rm -rf /usr/share/nginx/html/* &>>${log}
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>> download application content<<<<<<<<<<<\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
+func_exit_status
+
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl enable nginx
-systemctl start nginx
+
+echo -e "\e[36m>>>>>>>>>>>>>extract application content<<<<<<<<<<<\e[0m"
+unzip /tmp/frontend.zip &>>${log}
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>start nginx service<<<<<<<<<<<\e[0m"
+systemctl enable nginx &>>${log}
+systemctl start nginx &>>${log}
+func_exit_status
